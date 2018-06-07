dd2ef9617bdc66160c34f07ae59f4e54bf91a887
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-07 19:13:15 -0400

    add about-me and favourite pages

--- server.rb
+++ server.rb
@@ -1,13 +1,20 @@
 require 'sinatra'
 
 get '/home' do
-
   erb :index
-
 end
 
 get '/gallery' do
-
   erb :gallery
+end
 
+get '/about_me' do
+  @skills = ["Juggling", "Dancing", "CSS", "Ruby", "Cocktails"]
+  @interests = ["Photography", "Cooking", "Wine", "Surfing"]
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ["https://medium.com/", "https://www.blogto.com/", "https://www.netflix.com/browse", "http://ruby-doc.org/core-2.5.0/", "https://soundcloud.com/stream"]
+  erb :favourites
 end
